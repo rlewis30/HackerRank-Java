2b0f632215f682ad8924b7b9ab5bc1bb40a7012b
Ryan Lewis <ryan_lewis88@hotmail.com>
2018-04-18 23:10:38 -0400

    Completed a loop challenge

---
+++ Introduction Challenges/Java Loops I/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution
+{
+    public static void main(String[]args)
+    {
+        Scanner input = new Scanner(System.in);
+        int n=input.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n+" x "+i+" = "+n*i);
+        }
+    }
+}
---
+++ Introduction Challenges/Java Stdin and Stdout II/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution
+{
+    public static void main(String[]args)
+    {
+        Scanner input = new Scanner(System.in);
+        int i=input.nextInt();
+        double d=input.nextDouble();
+        String s=input.nextLine();
+        String s2=input.nextLine();
+        
+        System.out.println("String: " + s2);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
